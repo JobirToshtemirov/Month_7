835e07bc009a2c63dff10044cdd04f0415f2d2ce
Jobir <toshtemirovjobir6@gmail.com>
2024-10-24 19:36:13 +0500

    add

---
+++ lesson1/Mebel/common/views.py
@@ -0,0 +1,37 @@
+from django.contrib.auth import logout
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from common.forms import ContactUsModelForm
+
+
+def home_page_view(request):
+    return render(request, 'home.html')
+
+
+def contact_page_view(request):
+    return render(request, 'contact.html')
+
+
+def login_page_view(request):
+    return render(request, 'user-login.html')
+
+
+def register_page_view(request):
+    return render(request, 'user-register.html')
+
+
+def logout_page_view(request):
+    logout(request)
+    return redirect('common:home')
+
+
+def detail_page_view(request):
+    return render(request, 'detail.html')
+
+
+class ContactPageView(CreateView):
+    template_name = 'contact.html'
+    form_class = ContactUsModelForm
+    success_url = reverse_lazy("common:home")
---
+++ lesson2/accounts/views.py
@@ -0,0 +1,30 @@
+
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, f"You are now logged in as {user.email}")
+            return redirect('login')
+        else:
+            messages.error(request, "Invalid credentials")
+    return render(request, 'accounts/login.html')
